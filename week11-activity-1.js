5d1704171c3a71f96172d7b22be72e81c2ce4e56
williamGats <william.gatdula@olivarezcollegetagaytay.edu.ph>
2025-10-19 14:20:39 +0800

    William Gatdula activity

--- week11-activity-1.js
+++ week11-activity-1.js
@@ -26,3 +26,36 @@ again in the format: name / phone / email.
 Use the length property of the array to determine the index of the last element. 
 Remember that the array elements are indexed starting at 0
 */
+
+// #1 ADD a new contact.
+console.log("Problem #1");
+
+let contactAdd = contacts.push({name: "Maisie Haley", phone: "0913 531 3030", email: "risus.Quisque@urna.ca."});
+
+console.log(contacts);
+
+// #2 UPDATE the phone number for Helen Richards to: 0800 2222.
+
+console.log("Problem #2");
+
+contacts[2].phone = "0800 2222";
+
+console.log(contacts);
+
+// #3 DELETE the contact for Raja Villarreal from the array.
+console.log("Problem #3");
+
+contacts.splice(1, 1);
+
+console.log(contacts);
+
+// #4 Display the first and last contact
+console.log("Problem #4");
+
+let first = contacts[0];
+let last = contacts[contacts.length - 1];
+
+console.log(`First: ${first.name} / ${first.phone} / ${first.email}`);
+console.log(`Last: ${last.name} / ${last.phone} / ${last.email}`);
+
+// William Czyron A. Gatdula BSIT 211B
