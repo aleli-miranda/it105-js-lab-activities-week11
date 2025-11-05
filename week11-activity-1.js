eb3dddffb1de1006387c1f383fd7619bb191f32c
justinevistal <Justine.vistal@olivarezcollegetagaytay.edu.ph>
2025-11-05 10:23:49 +0800

    ako ay isang pogi

--- week11-activity-1.js
+++ week11-activity-1.js
@@ -26,3 +26,14 @@ again in the format: name / phone / email.
 Use the length property of the array to determine the index of the last element. 
 Remember that the array elements are indexed starting at 0
 */
+
+contacts[3] = { name: "Maisie Haley", phone:"0913 531 3030" , email: "risus.Quisque@urna.ca." };
+
+contacts[2].phone = ('0800 2222');
+
+contacts.pop(1)
+
+console.log(contacts[0]);
+
+console.log(contacts[2]);
+console.log("index of the last element: ",contacts.length)
